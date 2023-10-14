e70fd9d98f1fec26a9951c938633c0fc2c663be9
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-10-14 18:44:41 +0900

    add realm

---
+++ Realm/kotlin/main.kt
@@ -0,0 +1,45 @@
+﻿import io.realm.Realm
+import io.realm.RealmConfiguration
+
+fun main() {
+
+// Define the Realm configuration
+val config = RealmConfiguration.Builder()
+    .name("myrealm.realm")
+    .schemaVersion(1)
+    .build()
+
+// Initialize Realm
+val realm = Realm.getInstance(config)
+
+// Define a data model
+open class Person(
+    var name: String = "",
+    var age: Int = 0
+) : RealmObject()
+
+// Add data to Realm
+realm.executeTransaction { realm ->
+    val person = realm.createObject(Person::class.java)
+    person.name = "John"
+    person.age = 25
+}
+
+// Query data from Realm
+val results = realm.where(Person::class.java)
+    .equalTo("name", "John")
+    .findAll()
+
+// Update data in Realm
+realm.executeTransaction { realm ->
+    results[0]?.age = 26
+}
+
+// Delete data from Realm
+realm.executeTransaction { realm ->
+    results.deleteAllFromRealm()
+}
+
+// Close Realm when done
+realm.close()
+}
---
+++ Realm/swift/main.swift
@@ -0,0 +1,20 @@
+﻿let myDog = Dog()
+myDog.name = "Fido"
+myDog.age = 3
+
+// データを保存
+let realm = try! Realm()
+try! realm.write {
+    realm.add(myDog)
+}
+
+// データを取得
+let dogs = realm.objects(Dog.self)
+for dog in dogs {
+    print("\(dog.name) is \(dog.age) years old")
+}
+
+// データを削除
+try! realm.write {
+    realm.delete(myDog)
+}
